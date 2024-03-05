a75af0f507529659c7af1f298f31a3e3b1a07e29
duy vo <duyvo761@gmail.com>
2024-03-06 00:10:26 +0700

    update issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export const metadata = {
-  title: "App Router",
-};
+// export const metadata = {
+//   title: "App Router",
+// };
 
 export default function Page() {
   return <h1>App Router</h1>;
